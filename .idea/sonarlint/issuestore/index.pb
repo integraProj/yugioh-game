84d6dbaa66828de0b56271884abdb389b354f396
ViniCL <vinilodi2013@gmail.com>
2023-06-24 20:45:26 -0300

    Efeitos Sonoros
    Agora é emitido diferentes efeitos sonoros ao:
    
    - Levar dano.
    - Levar dano resultando em lifepoints zerados.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+q
+Asrc/main/java/eg/edu/guc/yugioh/cards/spells/CardDestruction.java,f\0\f006b62b404c5fb59c803e58ac28a825f660d4b2
+o
+?src/main/java/eg/edu/guc/yugioh/cards/spells/ChangeOfHeart.java,9\8\988f49e159e13c7de7bc8941bd9874a5e7001864
+_
+/src/main/java/eg/edu/guc/yugioh/cards/Card.java,7\8\78fca044c119b3a3a01ef0238045ff5852de9309
--- src/main/java/eg/edu/guc/yugioh/board/player/Player.java
+++ src/main/java/eg/edu/guc/yugioh/board/player/Player.java
@@ -2,6 +2,11 @@ package eg.edu.guc.yugioh.board.player;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 import eg.edu.guc.yugioh.cards.Card;
 import eg.edu.guc.yugioh.cards.Mode;
@@ -10,6 +15,7 @@ import eg.edu.guc.yugioh.cards.spells.SpellCard;
 import eg.edu.guc.yugioh.exceptions.IllegalSpellTargetException;
 import eg.edu.guc.yugioh.exceptions.MultipleMonsterAdditionException;
 import eg.edu.guc.yugioh.exceptions.UnexpectedFormatException;
+import eg.edu.guc.yugioh.configsGlobais.Logger;
 
 public class Player implements Duelist {
 
@@ -256,6 +262,43 @@ public class Player implements Duelist {
 
 	public void setLifePoints(int lifePoints) {
 		this.lifePoints = lifePoints;
+	}
+
+	public void takeDamage(int damage){
+
+		Logger.startLogs().info("Player - takeDamage Damage: " + damage );
+
+		int lp = getLifePoints();
+		setLifePoints(lp - damage);
+
+		Logger.startLogs().info("Player - takeDamage Lifepoints: " + getLifePoints() );
+
+		playDamageSong();
+
+
+	}
+
+	private void playDamageSong(){
+
+		Logger.startLogs().info("Player - playDamageSong");
+
+		String sourceSong;
+		if(getLifePoints() <=0 ) {
+			sourceSong = "src/main/resources/audios/gritoplayerlose.wav";
+		} else {
+			sourceSong = "src/main/resources/audios/gritoplayer.wav";
+		}
+
+		File musicPath = new File(sourceSong);
+
+		try{
+			AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInput);
+			clip.start();
+		} catch ( Exception e){
+
+			Logger.startLogs().error("Player - Exception: " + e + " " + musicPath.getName() );		}
 	}
 
 	public String getName() {
